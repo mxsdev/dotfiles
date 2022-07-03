c2a21e095f7c18de70299ea1d2658b0270be16dd
charburgx <charburgx@gmail.com>
2022-07-03 01:02:17 -0500

    show untracked files in telescope

--- nvim/.config/nvim/lua/user/core/telescope/custom-finders.lua
+++ nvim/.config/nvim/lua/user/core/telescope/custom-finders.lua
@@ -5,7 +5,9 @@ local _, builtin = pcall(require, "telescope.builtin")
 -- Smartly opens either git_files or find_files, depending on whether the working directory is
 -- contained in a Git repo.
 function M.find_project_files()
-  local ok = pcall(builtin.git_files)
+  local ok = pcall(builtin.git_files, {
+    show_untracked=true
+  })
 
   if not ok then
     builtin.find_files()
